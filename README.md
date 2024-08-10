f84f79edf7adf8f2177fb83546dc0b12253f18b7
scorpion963 <jackson20062008@gmail.com>
2024-08-10 17:11:21 -0500

    database setup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - [x] Make it deploy (vercel)
 - [x] Scaffold basic ui with mock data
-- [] Tidy up build process
+- [x] Tidy up build process
 - [] Actually set up a database (vercel postrgres)
 - [] Attach database to ui
 - [] Add authentiaction (w/ Clerk)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/dba791d4-db3b-46f3-ae7f-df64ebcd25e1-r97g75.jpg",
@@ -8,12 +9,20 @@ const mockUrls = [
 
 const mockImages = mockUrls.map((url, index) => ({id: index + 1, url}))
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts)
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map(image => (
-          <div key={image.id} className="w-1/4 p-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-1/4 p-4">
             <img src={image.url} />
           </div>
         ))}
